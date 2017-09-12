f701b27f73f847557e1769515213160a1f379950
bgillock <bitrate27@gmail.com>
2017-09-12 10:27:47 +0200

    Refactor cut

--- cut.js
+++ cut.js
@@ -1,90 +1,54 @@
-var cuttop;
-var cutTopCamera, cutScene, cutTopRenderer, cutTopControls;
-var cutfront;
-var cutFrontCamera, cutFrontRenderer, cutFrontControls;
-var cutright;
-var cutRightCamera, cutRightRenderer, cutRightControls;
+var topView
+var frontView
+var rightView
 var cutpersp;
 var cutPerspCamera, cutPerspRenderer, cutPerspControls;
+class CutView {
+    constructor(id, frustum, offsetLeft, offsetTop, up, position) {
+        var frustumSize = frustum
+        this.cut = document.getElementById(id)
+        var aspect = this.cut.clientWidth / this.cut.clientHeight
+        this.camera = new THREE.OrthographicCamera((frustumSize * aspect / -2) + offsetLeft,
+            (frustumSize * aspect / 2) + offsetLeft,
+            (frustumSize / -2) + offsetTop,
+            (frustumSize / 2) + offsetTop,
+            1, 2000)
+        this.camera.position.set(position.x, position.y, position.z)
+        this.camera.lookAt(0, 0, 0)
+        this.camera.up.fromArray(up)
+
+        this.renderer = new THREE.WebGLRenderer({ antialias: true })
+        this.renderer.setClearColor(0xf0f0f0);
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+        this.renderer.setSize(this.cut.clientWidth, this.cut.clientHeight)
+
+        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement, renderCut);
+        this.controls.enabled = true
+        this.controls.enableRotate = false
+
+        this.cut.appendChild(this.renderer.domElement)
+        this.cut.style.cursor = 'auto'
+    }
+}
+
 function loadCutScene(piece) {
     var bbox = new THREE.Box3().setFromObject(piece.group)
-    var size = new THREE.Vector3(bbox.max.x-bbox.min.x,bbox.max.y-bbox.min.y,bbox.max.z-bbox.min.z)
-    var center = new THREE.Vector3((bbox.max.x+bbox.min.x)/2,(bbox.max.y+bbox.min.y)/2,(bbox.max.z+bbox.min.z)/2)
-    
-    var frustumSize = size.x;
-    cuttop = document.getElementById('cuttop') 
-    var aspect = cuttop.clientWidth / cuttop.clientHeight;
-    cutTopCamera = new THREE.OrthographicCamera( (frustumSize * aspect / -2) - center.x , 
-                                                 (frustumSize * aspect / 2) - center.x, 
-                                                 (frustumSize / -2) - center.z, 
-                                                 (frustumSize / 2) - center.z,
-                                                 1, 2000);
-    cutTopCamera.position.set(0,-1000,0); // center x,z above y
-    cutTopCamera.lookAt(0,0,0);                                             
-    cutTopCamera.up.fromArray( [0,0,-1] ); 
- 
+    var size = new THREE.Vector3(bbox.max.x - bbox.min.x, bbox.max.y - bbox.min.y, bbox.max.z - bbox.min.z)
+    var center = new THREE.Vector3((bbox.max.x + bbox.min.x) / 2, (bbox.max.y + bbox.min.y) / 2, (bbox.max.z + bbox.min.z) / 2)
 
-    cutTopRenderer = new THREE.WebGLRenderer({ antialias: true });
-    cutTopRenderer.setClearColor(0xf0f0f0);
-    cutTopRenderer.setPixelRatio(window.devicePixelRatio);
-    cutTopRenderer.setSize(cuttop.clientWidth, cuttop.clientHeight);
-    
-    cutTopControls = new THREE.OrbitControls(cutTopCamera, cutTopRenderer.domElement, renderCut);
-    cutTopControls.enabled = true
-    cutTopControls.enableRotate = false
+    topView = new CutView('cuttop',
+        size.x, -center.x, -center.z, [0, 0, -1],
+        new THREE.Vector3(0, -1000, 0))
 
-    cuttop.appendChild(cutTopRenderer.domElement);
-    cuttop.style.cursor = 'auto';
-   
-    frustumSize = size.x;
-    cutfront = document.getElementById('cutfront') 
-    aspect = cutfront.clientWidth / cutfront.clientHeight;
-    cutFrontCamera = new THREE.OrthographicCamera( (frustumSize * aspect / -2) - center.x , 
-                                                 (frustumSize * aspect / 2) - center.x, 
-                                                 (frustumSize / -2) + center.y, 
-                                                 (frustumSize / 2) + center.y,
-                                                 1, 2000);
-    cutFrontCamera.position.set(0,0,-1000); // center x,z above y
-    cutFrontCamera.lookAt(0,0,0);                                             
-    cutFrontCamera.up.fromArray( [0,1,0] );  
+    frontView = new CutView('cutfront',
+        size.x, -center.x, center.y, [0, 1, 0],
+        new THREE.Vector3(0, 0, -1000))
 
-    cutFrontRenderer = new THREE.WebGLRenderer({ antialias: true });
-    cutFrontRenderer.setClearColor(0xf0f0f0);
-    cutFrontRenderer.setPixelRatio(window.devicePixelRatio);
-    cutFrontRenderer.setSize(cutfront.clientWidth, cutfront.clientHeight);
-    
-    cutFrontControls = new THREE.OrbitControls(cutFrontCamera, cutFrontRenderer.domElement, renderCut);
-    cutFrontControls.enabled = true
-    cutFrontControls.enableRotate = false
+    rightView = new CutView('cutright',
+        size.x, center.z, center.y, [0, 1, 0],
+        new THREE.Vector3(-1000, 0, 0))
 
-    cutfront.appendChild(cutFrontRenderer.domElement);
-    cutfront.style.cursor = 'auto';
-    
-    frustumSize = size.x;
-    cutright = document.getElementById('cutright') 
-    aspect = cutright.clientWidth / cutright.clientHeight;
-    cutRightCamera = new THREE.OrthographicCamera(  (frustumSize / -2) + center.z, 
-                                                 (frustumSize / 2) + center.z,
-                                                 (frustumSize / -2) + center.y, 
-                                                 (frustumSize / 2) + center.y,
-                                                 1, 2000);
-    cutRightCamera.position.set(-1000,0,0); 
-    cutRightCamera.lookAt(0,0,0);                                             
-    cutRightCamera.up.fromArray( [0,1,0] );  
-
-    cutRightRenderer = new THREE.WebGLRenderer({ antialias: true });
-    cutRightRenderer.setClearColor(0xf0f0f0);
-    cutRightRenderer.setPixelRatio(window.devicePixelRatio);
-    cutRightRenderer.setSize(cutright.clientWidth, cutright.clientHeight);
-    
-    cutRightControls = new THREE.OrbitControls(cutRightCamera, cutRightRenderer.domElement, renderCut);
-    cutRightControls.enabled = true
-    cutRightControls.enableRotate = false
-
-    cutright.appendChild(cutRightRenderer.domElement);
-    cutright.style.cursor = 'auto';
-    
-    cutpersp = document.getElementById('cutpersp') 
+    cutpersp = document.getElementById('cutpersp')
     aspect = cutpersp.clientWidth / cutpersp.clientHeight;
     cutPerspCamera = new THREE.PerspectiveCamera(45, aspect, 1, 10000);
     cutPerspCamera.position.set(size.x * 2, size.y * 2, size.z * 2);
@@ -94,16 +58,17 @@ function loadCutScene(piece) {
     cutPerspRenderer.setClearColor(0xf0f0f0);
     cutPerspRenderer.setPixelRatio(window.devicePixelRatio);
     cutPerspRenderer.setSize(cutpersp.clientWidth, cutpersp.clientHeight);
-    
+
     cutPerspControls = new THREE.OrbitControls(cutPerspCamera, cutPerspRenderer.domElement, renderCut);
     cutPerspControls.enabled = true
     cutPerspControls.enableRotate = true
 
     cutpersp.appendChild(cutPerspRenderer.domElement);
     cutpersp.style.cursor = 'auto';
-     
+
 }
-function initCutScene() {    
+
+function initCutScene() {
     cutScene = new THREE.Scene();
 
     // Lights
@@ -112,17 +77,18 @@ function initCutScene() {
     var directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(1, 0.75, 0.5).normalize();
     cutScene.add(directionalLight);
-   
+
     document.addEventListener('keydown', onDocumentKeyDown, false);
     document.addEventListener('keyup', onDocumentKeyUp, false);
     window.addEventListener('resize', onWindowResize, false);
-    
+
     return cutScene
 }
+
 function renderCut() {
-    if (cutTopCamera == null || cutFrontCamera == null || cutRightCamera == null || cutPerspCamera == null) return
-    cutTopRenderer.render(cutScene, cutTopCamera)
-    cutFrontRenderer.render(cutScene, cutFrontCamera)
-    cutRightRenderer.render(cutScene, cutRightCamera)
+    if (topView == null || frontView == null || rightView == null || cutPerspCamera == null) return
+    topView.renderer.render(cutScene, topView.camera)
+    frontView.renderer.render(cutScene, frontView.camera)
+    rightView.renderer.render(cutScene, rightView.camera)
     cutPerspRenderer.render(cutScene, cutPerspCamera)
 }
